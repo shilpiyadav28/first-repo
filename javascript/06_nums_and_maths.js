54399f2f7799f4ed8c80232626775cc877baa173
shilpiyadav28 <shilpiy2811@gmail.com>
2024-10-03 18:09:46 +0000

    num and maths file

--- javascript/06_nums_and_maths.js
+++ javascript/06_nums_and_maths.js
@@ -25,7 +25,25 @@ console.log(total.toLocaleString('en-IN'));// yeh numbers ma comma provide jrne 
 
 //++++++++++++++++ MTHS+++++++++
 
+//Math is also a function of JS
 
+// console.log(Math);
+// console.log(Math.abs(-4));//abosolute value : coverts any negative number into positive
+// console.log(Math.round(4.6));// round off
+// console.log(Math.ceil(4.2));// round off but takes upper value eg. here it will take 5
+// console.log(Math.floor(4.9));// round off but takes lower value, here it takes 4
+// console.log(Math.min(4, 3, 6, 8));// gives min value=3
+// console.log(Math.max(4, 3, 6, 8));// gives max=8
 
+console.log(Math.random());//gives any random value from 0 to 1
+console.log((Math.random()*10) + 1);// but we want minimum value should be one not 0, thus we multiply by 10 and add 1
+console.log(Math.floor(Math.random()*10) + 1); // as we want roundoff as well so using math.floor
 
+//case like we want number in between a rage, so we sepcify the min and max values
 
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// here we need value from 10-20 only so, Math.random()--we get any value. thus, to get our values we set 
+//console.log(Math.floor(Math.random() * (max - min + 1)) + min)
