85bd5651f2667f901a5244d8eae50498b99c90db
shilpiyadav28 <shilpiy2811@gmail.com>
2024-12-09 09:46:35 +0000

    learn DOM with its basics

--- javascript/05_basics/script.js
+++ javascript/05_basics/script.js
@@ -1,1 +1,19 @@
-console.log("hello world!");
+//alert("connected") only for learning purpose, this is very wired in production.
+//  let hold = document.getElementById("changetextbutton");
+//  console.log(hold);    ---this to check what we are holding means which content we have fetched for the process
+
+// example -1 is done
+document.getElementById("changetextbutton")
+.addEventListener("click", function(){
+  let paragraph = document.getElementById("mypara");
+  paragraph.textContent = "Paragraph is Changed!";
+});
+
+// example -2
+document.getElementById("highlightfirstcity").addEventListener("click" , function () {
+   let highlightedcity =  document.getElementById("citilist");
+   highlightedcity.firstElementChild.classList.add("highlight");   //---- by using classList we can add our CSS by giving class name in it
+});
+
+
+  
