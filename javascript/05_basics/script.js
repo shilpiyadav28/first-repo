1eb50ad9a5c1eee0cf41757147ac4464a4efd9b0
shilpiyadav28 <shilpiy2811@gmail.com>
2024-12-09 14:45:24 +0000

    DOM learning carried

--- javascript/05_basics/script.js
+++ javascript/05_basics/script.js
@@ -9,11 +9,43 @@ document.getElementById("changetextbutton")
   paragraph.textContent = "Paragraph is Changed!";
 });
 
-// example -2
+// example -2 // traversing the content
 document.getElementById("highlightfirstcity").addEventListener("click" , function () {
    let highlightedcity =  document.getElementById("citilist");
    highlightedcity.firstElementChild.classList.add("highlight");   //---- by using classList we can add our CSS by giving class name in it
 });
 
 
+//example -3 //changing/ manipulating the content
+document.getElementById("changeItem").addEventListener("click", function (){
+   let order = document.getElementById("chaiType");
+   order.textContent = "Expresso";
+   
+})
+
+
+//example -4 
+//creating and inserting elements
+
+// document.getElementById("addNewitem").addEventListener("click" , function () {
   
+//   let newitem = document.createElement('li');
+//   newitem.textContent = "Eggs";
+  
+//   document.getElementById("shoppinglist").appendChild("newitem");
+// });
+document.getElementById("addNewitem").addEventListener("click" , function () {
+  const  changedItem = document.createElement('li');
+  changedItem.textContent = "Eggs";
+
+const addingchild = document.getElementById("shoppinglist");
+addingchild.appendChild("changedItem");
+});
+  
+
+//example-5 --- removing the elements
+
+document.getElementById("removetask").addEventListener("click" , function () {
+  let newtasklist = document.getElementById("tasking");
+  newtasklist.lastElementChild.remove(newtasklist);
+})
